a1d044576dca3085f68f8f81f86d7b003701dd02
Matt Doren <mattdoren62@gmail.com>
2020-04-29 13:24:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/profilekotlin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.profilekotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnProfile = findViewById<Button>(R.id.idBtnViewProfile)
+        btnProfile.setOnClickListener {
+
+            var intent = Intent(this,Main2Activity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
